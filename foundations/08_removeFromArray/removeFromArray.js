3d481592ced16a0deb545261aa5e38532707dbf0
Annie <annieweimail2023@gmail.com>
2026-06-25 17:40:35 -0400

    Complete exercises 08 to 11

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(arr, ...args) {
+
+    let newArr = [];
+
+    arr.forEach((item) => {
+        if(!args.includes(item)) {
+            newArr.push(item);
+        }
+    });
+
+    return newArr;
+    
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
