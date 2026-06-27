aa3754f04cb225fbcd036ffea1d99b3a6f9b6479
Annie <annieweimail2023@gmail.com>
2026-06-27 16:27:39 -0400

    Complete exercse 16_findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,35 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+
+    const sorted = people.sort((a, b) => {
+        const currentYear = new Date().getFullYear();
+        const ageA = (a.yearOfDeath || currentYear) - a.yearOfBirth;
+        const ageB = (b.yearOfDeath || currentYear) - b.yearOfBirth;
+        return ageA > ageB ? -1 : 1;
+    })
+
+    return sorted[0];
 
 };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
+// an interesting way to use reduce!
+
+// const getAge = function (birth, death) {
+//   if (!death) {
+//     death = new Date().getFullYear();
+//   }
+//   return death - birth;
+// };
+
+// const findTheOldest = function (people) {
+//   return people.reduce((oldest, currentPerson) => {
+//     const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+//     const currentAge = getAge(
+//       currentPerson.yearOfBirth,
+//       currentPerson.yearOfDeath
+//     );
+//     return oldestAge < currentAge ? currentPerson : oldest;
+//   });
+// };
