d2872d66761b4014d92d02d373d7b9ab1087b127
Annie <annieweimail2023@gmail.com>
2026-06-27 15:59:44 -0400

    Complete exercise 13_palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const noPunc = str
+        .split("")
+        .filter(letter => (letter.toLowerCase() !== letter.toUpperCase()) || (/\d/.test(letter)))
+        .join("");
+    const reversed = noPunc.split("").reverse().join("");
+    
+    return reversed.toLowerCase() === noPunc.toLowerCase();
 
+    // or just use filter((letter) => alphanumerical.includes(letter)) !!!
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+
